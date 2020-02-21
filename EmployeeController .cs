5e466316b9cd3e398ad38495ff27c1c5ae48ee19
DDerick <49862552+DDerick@users.noreply.github.com>
2020-02-21 15:39:29 +0300

    Add files via upload
    This is a simple .net core mvc application using asp identity

---
+++ EmployeeController .cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Authentication.Models;
+
+namespace Authentication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDAL employeeDAL = new EmployeeDAL();
+        public IActionResult Index()
+        {
+            List<Employee> empList = new List<Employee>();
+            empList = employeeDAL.GetAllEmployee().ToList();
+            return View(empList);
+        }
+
+        //For my review [http]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Create([Bind] Employee objEmp)
+        {
+            if (ModelState.IsValid)
+            {
+                employeeDAL.AddEmployee(objEmp);
+                return RedirectToAction("Index");
+            }
+            return View(objEmp);
+        }
+
+
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Employee emp = employeeDAL.GetEmployeeById(id);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            return View(emp);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int? id, [Bind] Employee objEmp)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                employeeDAL.UpdateEmployee(objEmp);
+                return RedirectToAction("Index");
+            }
+            return View(employeeDAL);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Employee emp = employeeDAL.GetEmployeeById(id);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            return View(emp);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Employee emp = employeeDAL.GetEmployeeById(id);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            return View(emp);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteEmp(int? id)
+        {
+            employeeDAL.DeleteEmployee(id);
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ EmployeeDAL.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.Models
+{
+    public class EmployeeDAL
+    {
+        string connectionString = "Data Source=DESKTOP-NOPF2G5; Initial Catalog=EMPLOYEEPRAC;Persist Security Info=False; User ID=people2020;password=123";
+        //GET ALL
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            List<Employee> empList = new List<Employee>();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_GetAllEmployee", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    Employee emp = new Employee();
+                    emp.ID = Convert.ToInt32(dr["ID"].ToString());
+                    emp.Name = dr["Name"].ToString();
+                    emp.Gender = dr["Gender"].ToString();
+                    emp.Company = dr["Company"].ToString();
+                    emp.Department = dr["Department"].ToString();
+
+                    empList.Add(emp);
+                }
+                con.Close();
+            }
+            return empList;
+        }
+
+        //To insert Employee
+        public void AddEmployee(Employee emp)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_InsertEmployee", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@Gender", emp.Gender);
+                cmd.Parameters.AddWithValue("@Company", emp.Company);
+                cmd.Parameters.AddWithValue("@Department", emp.Department);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+        }
+
+        //To Update Employee
+        public void UpdateEmployee(Employee emp)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_UpdateEmployee", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@EmpId", emp.ID);
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@Gender", emp.Gender);
+                cmd.Parameters.AddWithValue("@Company", emp.Company);
+                cmd.Parameters.AddWithValue("@Department", emp.Department);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        // To Delete Employee
+        public void DeleteEmployee(int? empId)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_DeleteEmployee", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@EmpId", empId);
+
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        //Get Employee by ID
+        public Employee GetEmployeeById(int? empId)
+        {
+            Employee emp = new Employee();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_GetEmployeeById", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@EmpId", empId);
+
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+
+                    emp.ID = Convert.ToInt32(dr["ID"].ToString());
+                    emp.Name = dr["Name"].ToString();
+                    emp.Gender = dr["Gender"].ToString();
+                    emp.Company = dr["Company"].ToString();
+                    emp.Department = dr["Department"].ToString();
+
+
+                }
+                con.Close();
+            }
+            return emp;
+        }
+
+
+    }
+}
